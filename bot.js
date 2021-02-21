cb3fe820d9a599936150dd41bd7a32cb0da212f7
Easton Kang <euisungkang@easton.local>
2021-02-21 06:05:14 -0600

    Basic intro

--- bot.js
+++ bot.js
@@ -1,32 +1,26 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
 
- 
+//let channel = process.env.CHANNELID;
 
 client.on('message', message => {
 
+    let channel = client.channels.find(channel => channel.name === 'general')
+
     if (message.content == '#ping') {
-       message.channel.send('pong');
+       channel.send('pong');
     }
     else if (message.content == '#pong') {
-        message.channel.send('ping');
+        channel.send('ping');
     }
     else if (message.content == '#introduce') {
-        message.channel.send('Wear a Mask, Stay Safe')
+        channel.send('Sup Bitch')
     }
 
 });
- 
 
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
