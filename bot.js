096af5a81c4af84ba37d948855eb4c188aff1cda
Easton Kang <euisungkang@easton.local>
2021-02-22 19:48:03 -0600

    voiceStateUpdate

--- bot.js
+++ bot.js
@@ -7,8 +7,11 @@ client.on('ready', () => {
 
 //let channel = process.env.CHANNELID;
 
+client.login(process.env.BOT_TOKEN);
+
 client.on('message', message => {
 
+    //Bot Central General: 813017396553449472
     let channel = client.channels.find(channel => channel.id === '813017396553449472')
 
     if (message.content == '#ping') {
@@ -18,9 +21,21 @@ client.on('message', message => {
         channel.send('ping');
     }
     else if (message.content == '#introduce') {
-        channel.send('Sup Bitch')
+        channel.send('Wht')
     }
 
 });
 
-client.login(process.env.BOT_TOKEN);
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    //console.log("something happened");
+
+    let oldUserChannel = oldMember.voiceChannel;
+    let newUserChannel = newMember.voiceChannel;
+
+    if (oldUserChannel === undefined && newUserChannel !== undefined) {
+        console.log('Joined Channel: ' + newMember.nickname)
+
+    } else if (newUserChannel === undefined) {
+        console.log('Left Channel: ' + newMember.nickname)
+    }
+});
